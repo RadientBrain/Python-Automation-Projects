b1572987a1151dc0d9f5de494134c57f3e06bdb6
RadientBrain <maheshwarichetan00@gmail.com>
2020-09-08 03:02:59 +0530

    Logo Paster Automation

--- Logo Paster/main.py
+++ Logo Paster/main.py
@@ -1,4 +1,4 @@
-import os ,shutil, time
+import os ,shutil, time,sys
 from distutils.dir_util import copy_tree
 import tkinter as tk
 import pygame
@@ -10,9 +10,11 @@ class App(tk.Tk):
     file_extn = ('.jpg','.jpeg','.png')
     
     def __init__(self, *args, **kwargs):
+
+
         pygame.mixer.init()
         t_pth = os.getcwd()
-        pygame.mixer.music.load(r'{}/music/bg.mp3'.format(t_pth))
+        pygame.mixer.music.load(r'{}\music\bg.mp3'.format(t_pth))
         pygame.mixer.music.set_volume(1)
         pygame.mixer.music.play(loops=-1)
 
@@ -57,12 +59,61 @@ class App(tk.Tk):
 
 
         #Start Process
-        self.lbl_start = LabelFrame(self,bg='#005082',fg="#fff",relief=tk.RIDGE,labelanchor="n")
-        self.lbl_start.grid(row=3,column=0,padx=self.xx//4)
+        self.lbl_start = LabelFrame(self,bg='#005082',fg="#fff",relief=tk.RIDGE,labelanchor="n",bd=0)
+        self.lbl_start.grid(row=3,column=0)
         self.start_btn = Button(self.lbl_start,bg='#005082',fg="#fff", text='Start Process',
                                      command=self.start_process,relief=tk.RAISED)
-        self.start_btn.grid(sticky="n e w s", row=0)
+        self.start_btn.grid(sticky="n e w s", row=0,column=0,padx=self.xx//32)
 
+        #Next Process
+        self.next_btn = Button(self.lbl_start,bg='#005082',fg="#fff", text='Next Process',
+                                     command=self.next_process,relief=tk.RAISED)
+        self.next_btn.grid(sticky="n e w s", row=0,column=1,padx=self.xx//32)
+
+        try:
+            os.remove(f"{os.getcwd()}/wtrmk_dir/logo.png")
+
+
+        except Exception as e:
+            self.next_process()
+
+        try:
+            folder = f"{os.getcwd()}/dst_photos"
+            for filename in os.listdir(folder):
+                file_path = os.path.join(folder, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print('Failed to delete %s. Reason: %s' % (file_path, e))
+        except Exception as e:
+            self.next_process()
+
+    def next_process(self):
+
+        try:
+            self.progress_lbl.config(text="",width=40,height=2)
+            os.remove(f"{os.getcwd()}/wtrmk_dir/logo.png")
+        except Exception as e:
+            # print("inside next process")
+            pass
+
+        try:
+            folder = f"{os.getcwd()}/dst_photos"
+            for filename in os.listdir(folder):
+                file_path = os.path.join(folder, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print('Failed to delete %s. Reason: %s' % (file_path, e))
+        except Exception as e:
+            # print("inside next process")
+            pass
 
 
     def start_process(self):
@@ -72,7 +123,7 @@ class App(tk.Tk):
         # #Label for progress
         self.lbl_progress = LabelFrame(self,bg='#005082',bd=0,labelanchor="n")
         self.lbl_progress.grid(row=4,column=0,padx=self.xx//4,pady = self.yy//32)
-        self.progress_lbl= Label(self.lbl_progress,bg='#005082',fg="#fff", text='Task in Progress...')
+        self.progress_lbl= Label(self.lbl_progress,bg='#005082',fg="#fff", text='Task in Progress...',width=40,height=2)
         self.progress_lbl.grid(sticky="n e w s", row=0)
         self.update()
 
@@ -82,11 +133,11 @@ class App(tk.Tk):
         
         if(Flag):
             time.sleep(1)
-            self.progress_lbl.config(text="Please select an Image source directory first!",width=40)
+            self.progress_lbl.config(text="Please select a valid Image source directory first!\nThis might be empty!",width=40,height=2)
             Flag=0
         else:
             # self.progress_lbl.grid_remove()
-            self.progress_lbl.config(text='Task Completed!',width=40)
+            self.progress_lbl.config(text='Task Completed!',width=40,height=2)
 
         
 
@@ -99,14 +150,28 @@ class App(tk.Tk):
             copy_tree(self.src_file_dir, self.src_photos_dir)
 
     def open_wtrmk_dir(self):
+        
         self.wtrmk_from_user = filedialog.askopenfilename(initialdir=os.getcwd())
         # print(self.wtrmk_from_user)
         self.dstn_photos_dir = f"{os.path.dirname(os.path.abspath(__file__))}/{'wtrmk_dir'}"
         if(self.wtrmk_from_user==""):
             messagebox.showinfo("No Watermark Image Selected","Please select an Image as watermark!")
         else:
-            shutil.copy(self.wtrmk_from_user, self.dstn_photos_dir)
-            os.rename(f"{self.dstn_photos_dir}/{os.path.basename(self.wtrmk_from_user)}",f"{os.path.dirname(self.dstn_photos_dir)}/wtrmk_dir/logo.png")
+            try:
+                shutil.copy(self.wtrmk_from_user, self.dstn_photos_dir)
+                self.update()
+                os.rename(f"{os.getcwd()}/wtrmk_dir/{os.path.basename(self.wtrmk_from_user)}",f"{os.getcwd()}/wtrmk_dir/logo.png")
+            except Exception as e:
+                if sys.hexversion < 0x03000000:
+                    if isinstance(e, shutil.Error) and "same file" in str(e):
+                        pass
+                elif isinstance(e, shutil.SameFileError):
+                        pass
+                else:
+                    if "file already exists" in str(e):
+                        messagebox.showerror("Error!","Please Click on Next button to proceed!")
+                raise # Must be another error 
+            
 
     def run(self):
         self.mainloop()
@@ -119,12 +184,14 @@ class AutomatePhotoshopWatermark(App):
         self.src_photos = f"{os.path.dirname(os.path.abspath(__file__))}/{'src_photos'}"
         self.dst_photos = f"{os.path.dirname(os.path.abspath(__file__))}/{'dst_photos'}"
 
+        try:
+            self.wtrmk_img = Image.open(f"{os.getcwd()}/wtrmk_dir/logo.png").convert("RGBA")
 
-        self.wtrmk_img = Image.open(f"{os.getcwd()}/wtrmk_dir/logo.png")
-
-        self.wtrmk_img_width = self.wtrmk_img.width
-        self.wtrmk_img_height = self.wtrmk_img.height
-
+            self.wtrmk_img_width = self.wtrmk_img.width
+            self.wtrmk_img_height = self.wtrmk_img.height
+        except Exception as e:
+            # print('logo file not there')
+            pass
     
     def run(self):
         global Flag
